568a5f5b6ec1fad89a9a05e92d240331504c9348
Christian Becher <phaziz@gmail.com>
2017-03-02 20:34:34 +0100

    Update 2017-02-20-Typedef.c

--- 2017-02-20-Typedef.c
+++ 2017-02-20-Typedef.c
@@ -1,12 +1,3 @@
-/*
- ============================================================================
- Name        : typedefBsp.c
- Author      : René Schülke
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 #include <stdlib.h>
