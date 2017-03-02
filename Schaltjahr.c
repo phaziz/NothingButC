7824be322f87fc43a3ae5596d9f308387d51f092
Christian Becher <phaziz@gmail.com>
2017-03-02 20:40:52 +0100

    Update Schaltjahr.c

--- Schaltjahr.c
+++ Schaltjahr.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : Schaltjahr.c
- Author      : René Schülke
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
