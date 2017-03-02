3759039ccbfd1f9ef3958c88b549547aa86eaaa7
Christian Becher <phaziz@gmail.com>
2017-03-02 20:38:08 +0100

    Update Fahrstuhl.c

--- Fahrstuhl.c
+++ Fahrstuhl.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : Fahrstuhl.c
- Author      : René Schülke
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
